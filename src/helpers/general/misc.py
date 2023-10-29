c3ffae6e0a4b2c46b0762f7e5936145c7133ca26
Cuh4 <roozzguy@gmail.com>
2023-10-29 18:13:54 +0000

    categorization annotations + new function

--- src/helpers/general/misc.py
+++ src/helpers/general/misc.py
@@ -3,5 +3,12 @@
 # // ---------------------------------------------------------------------
 
 # // ---- Main
+# // String
 def doesStringOnlyContainLetter(string: str, letters: str):
     return set(string) <= set(letters)
+
+def truncateIfTooLong(string: str, maxLength: int, ending: str = "..."):
+    if len(string) > maxLength:
+        return string[: maxLength - len(ending)] + ending
+    
+    return string
