53530b1771d3586acf1a5b1340f239077c1c9bf3
Cuh4 <roozzguy@gmail.com>
2023-10-29 16:30:42 +0000

    new config params

--- src/example_config.py
+++ src/example_config.py
@@ -7,5 +7,7 @@ chatCooldown = 4 # cooldown given to each user. if a user tries to talk to the b
 activityText = "you." # in discord, this becomes: "Watching you."
 loadingEmoji = "<a:loadingv2:1167901026687393812>" # emoji to use for loading response message. use "\:(emoji name):" in a discord channel to get an emoji's id
 maxResponseLength = 700 # character limit of bot response
+allowProfanity = False # whether or not to allow the bot to send messages that contain profanity, and whether or not to interact with messages that contain profanity
+maxQueryLength = 150 # character limit of user messages
 
 # [!!] Rename to config.py [!!]
