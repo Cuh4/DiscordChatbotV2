bcffb9389a8b56a0aa98fd061446b683182d57ae
Cuh4 <roozzguy@gmail.com>
2023-10-29 10:49:50 +0000

    new config parameter

--- src/example_config.py
+++ src/example_config.py
@@ -6,5 +6,6 @@ botToken = "" # token of your discord bot. create a bot here: https://discord.co
 chatCooldown = 4 # cooldown given to each user. if a user tries to talk to the bot twice in {chatCooldown} seconds, the query will not be processed
 activityText = "you." # in discord, this becomes: "Watching you."
 loadingEmoji = "<a:loadingv2:1167901026687393812>" # emoji to use for loading response message. use "\:(emoji name):" in a discord channel to get an emoji's id
+maxResponseLength = 700 # character limit of bot response
 
 # [!!] Rename to config.py [!!]
