1e5647234c53e6ae41accb0dd9f350cd5b3c1485
Cuh4 <roozzguy@gmail.com>
2023-10-29 09:23:15 +0000

    new slash command to restart the bot

---
+++ src/slashCommands/restart.py
@@ -0,0 +1,33 @@
+# // ---------------------------------------------------------------------
+# // ------- [Discord Chatbot v2] Restart Slash Command
+# // ---------------------------------------------------------------------
+
+# // Imports
+import discord
+from discord import app_commands
+import os
+
+from helpers import discord as discordHelpers
+import config
+
+# // Main
+def command(client: discord.Client, tree: discord.app_commands.CommandTree):
+    # slash command
+    @app_commands.command(
+        name = "restart",
+        description = "Restart the bot."
+    )
+    async def command(interaction: discord.Interaction):
+        # check if the user running this command is the person who created the bot
+        if not discordHelpers.utils.isBotOwner(client, interaction.user):
+            return await interaction.response.send_message(
+                embed = discordHelpers.embeds.failure(f"Invalid permissions.")
+            )
+        
+        # restart the bot
+        await interaction.response.send_message(
+            embed = discordHelpers.embeds.success(f"{config.loadingEmoji} Restarting...")
+        )
+        
+        os.system("start py main.py")
+        exit(0)
