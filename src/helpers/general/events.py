d681e0b71489e29e9519eb3b34639ec8f5f9f914
Cuh4 <roozzguy@gmail.com>
2023-11-06 02:25:36 +0000

    new helper module woohoo

---
+++ src/helpers/general/events.py
@@ -0,0 +1,92 @@
+# // ---------------------------------------------------------------------
+# // ------- [Discord Chatbot v2] Events
+# // ---------------------------------------------------------------------
+
+# // ---- Imports
+import inspect
+
+# // ---- Variables
+events = {}
+
+# // ---- Functions
+def isCoroutine(func):
+    return inspect.iscoroutinefunction(func)
+
+# // ---- Main
+# // Event
+# contains functions. when an event is called, all of its functions (callbacks) are called
+class event:
+    def __init__(self, name: str):
+        self.name = name
+        self.callbacks: list["callback"] = []
+        self.callbackID = 0
+        
+    def fire(self, *args, **kwargs):
+        returnValue = None
+        
+        for callback in self.callbacks:
+            if callback.isAsync:
+                continue
+            
+            returnValue = callback.call(*args, **kwargs)
+            
+        return returnValue
+    
+    async def asyncFire(self, *args, **kwargs):
+        returnValue = None
+        
+        for callback in self.callbacks:
+            if not callback.isAsync:
+                continue
+            
+            returnValue = await callback.asyncCall(*args, **kwargs)
+            
+        return returnValue
+    
+    def attach(self, func):
+        self.callbackID += 1
+
+        currentCallback = callback(
+            func = func,
+            id = self.callbackID
+        )
+        
+        currentCallback.attach(self)
+
+        return currentCallback
+    
+    def detach(self, callback: "callback"):
+        callback.detach()
+        return self
+        
+# function with extra steps
+class callback:
+    def __init__(self, func, id: int):
+        self.func = func
+        self.id = id
+        self.parent: "event" = None
+        self.isAsync = isCoroutine(func)
+        
+    def attach(self, event: "event"):
+        self.parent = event
+        self.parent.callbacks.append(self)
+        return self
+        
+    def detach(self):
+        if self.parent is None:
+            return self
+        
+        self.parent.callbacks.remove(self)
+        return self
+        
+    def call(self, *args, **kwargs):
+        if self.isAsync:
+            return
+        
+        return self.func(*args, **kwargs)
+    
+    async def asyncCall(self, *args, **kwargs):
+        if not self.isAsync:
+            return
+        
+        return await self.func(*args, **kwargs)
