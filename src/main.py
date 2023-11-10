73850caeb289e5d6fa4c497e3f6f915b0ef25395
Cuh4 <roozzguy@gmail.com>
2023-11-10 19:01:16 +0000

    always show footer + show response confidence

--- src/main.py
+++ src/main.py
@@ -196,8 +196,7 @@ async def on_message(message: discord.Message):
             color = discord.Colour.from_rgb(125, 255, 125)
         )
         
-        if source != "Built-In":
-            responseEmbed.set_footer(text = f"Response produced by {source} | Response may be inaccurate.", icon_url = message.author.display_avatar.url)
+        responseEmbed.set_footer(text = f"Response produced by {source} | Response may be inaccurate. | Response Confidence: {response.getResponseConfidence() * 100}%", icon_url = message.author.display_avatar.url)
 
         # reply with response
         feedbackView = ui.views.wrap(
