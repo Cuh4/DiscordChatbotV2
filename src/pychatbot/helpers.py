4bb14ad53e4e8e06cdd24800d590bf048f0f0b85
Cuh4 <roozzguy@gmail.com>
2023-11-12 17:42:19 +0000

    dont modify provided string

--- src/pychatbot/helpers.py
+++ src/pychatbot/helpers.py
@@ -22,14 +22,15 @@ def isTextProfane(string: str) -> bool:
     return filter.contains_profanity(string) or string.find("https://") != -1 or string.find("http://") != -1
 
 def censorProfaneText(string: str, linkCensorText: str = "[Censored Link]", generalCensorCharacter: str = "*") -> str:
-    string = re.sub(
+    newString = re.sub(
         pattern = "https?://\S+|www\.\S+|\S+\.\S+/\S+|\S+\?(\S+=\S+&?)*\S+", # remove links (source: chatgpt)
         repl = linkCensorText, 
         string = string, 
         flags = re.MULTILINE
     )
-
-    return filter.censor(string, generalCensorCharacter)
+    
+    newString = filter.censor(newString, generalCensorCharacter)
+    return newString
 
 def clamp(num: float|int, min: float|int, max: float|int):
     if num < min:
