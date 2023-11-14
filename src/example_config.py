d42587019b618f2e911e94c6e57bfd5853e2e406
Cuh4 <roozzguy@gmail.com>
2023-11-14 00:12:26 +0000

    separate config by category

--- src/example_config.py
+++ src/example_config.py
@@ -2,13 +2,20 @@
 # // ------- [Discord Chatbot v2] Config
 # // ---------------------------------------------------------------------
 
+# // discord bot
 botToken = "" # token of your discord bot. create a bot here: https://discord.com/developers/applications'. be sure to enable message content intents
-chatCooldown = 4 # cooldown given to each user. if a user tries to talk to the bot during the cooldown of {chatCooldown} seconds, the user will be ignored
 activityText = "you." # in discord, this becomes: "Watching you."
-loadingEmoji = "<a:loadingv2:1167901026687393812>" # emoji to use for loading response message. use "\:(emoji name):" (eg: "\:some_custom_emoji:") in discord to get an emoji's id
-maxResponseLength = 700 # character limit of bot response
+
+# // chatbot specific
 allowProfanity = False # whether or not to allow the bot to send messages that contain profanity, and whether or not to interact with messages that contain profanity
-maxQueryLength = 150 # character limit of user messages
+chatCooldown = 4 # cooldown given to each user. if a user tries to talk to the bot during the cooldown of {chatCooldown} seconds, the user will be ignored
+
+# // ids
+loadingEmoji = "<a:loadingv2:1167901026687393812>" # emoji to use for loading response message. use "\:(emoji name):" (eg: "\:some_custom_emoji:") in discord to get an emoji's id
 responseReportsChannelID = 1234567 # if a user presses the "report response" button for a response, it will show up in the channel with this id
+
+# // character limits
+maxResponseLength = 700 # character limit of bot response
+maxQueryLength = 150 # character limit of user messages
 
 # [!!] Rename to config.py [!!]
