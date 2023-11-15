6debd3e0c90249246c59fb3acca8012956b545b3
Cuh4 <roozzguy@gmail.com>
2023-11-15 14:04:53 +0000

    view class with default timeout behaviour

--- src/ui/views/__init__.py
+++ src/ui/views/__init__.py
@@ -5,8 +5,27 @@
 # // Imports
 import discord
 from .feedbackView import view as feedback
+from .failedResponseView import view as failedResponse
 
 # // Functions
 def wrap(message: discord.Message, view: discord.ui.View):
     view.message = message
     return view
+
+class view(discord.ui.View):
+    async def on_timeout(self):
+        # disable items
+        for item in self.children:
+            # disable all buttons
+            if isinstance(item, discord.ui.Button):
+                # ignore url buttons
+                if item.style == discord.ButtonStyle.url:
+                    continue
+                
+                # disable button
+                item.disabled = True
+    
+        # save changes
+        return await self.message.edit(
+            view = self
+        )
