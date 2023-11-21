548281b1f884765c8c51f663aef87e841757bec8
Cuh4 <roozzguy@gmail.com>
2023-11-21 07:11:51 +0000

    function is now a template method

--- src/ui/views/__init__.py
+++ src/ui/views/__init__.py
@@ -8,8 +8,3 @@ import discord
 from .template import view as template
 from .responseView import view as response
 from .failedResponseView import view as failedResponse
-
-# // ---- Functions
-def wrap(message: discord.Message, view: discord.ui.View):
-    view.message = message
-    return view
