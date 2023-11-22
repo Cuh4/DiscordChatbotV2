22a981b128af080fd77253e338cc4191d9f53a12
Cuh4 <roozzguy@gmail.com>
2023-11-22 15:17:05 +0000

    added annotations

--- src/helpers/general/events.py
+++ src/helpers/general/events.py
@@ -1,5 +1,5 @@
 # // ---------------------------------------------------------------------
-# // ------- [Discord Chatbot v2] Events
+# // ------- [Record] Events Module
 # // ---------------------------------------------------------------------
 
 # // ---- Imports
@@ -15,11 +15,11 @@ def isCoroutine(func: "function"):
 def getSavedEvent(name: str):
     return events.get(name, None)
 
-# // ---- Main
+# // ---- Classes
 # // Event
 # contains functions. when an event is called, all of its functions (callbacks) are called
 class event:
-    def __init__(self, name: str):
+    def __init__(self, name: str = ""):
         self.name = name
         self.callbacks: list["callback"] = []
         self.callbackID = 0
@@ -35,69 +35,95 @@ class event:
     def fire(self, *args, **kwargs):
         returnValue = None
         
+        # go through all regular functions and call them
         for callback in self.callbacks:
             if callback.isAsync:
                 continue
             
             returnValue = callback.call(*args, **kwargs)
             
+        # returns the most recent return value
         return returnValue
     
     async def asyncFire(self, *args, **kwargs):
         returnValue = None
         
+        # go through all callbacks that are coroutines and call them
         for callback in self.callbacks:
             if not callback.isAsync:
                 continue
             
             returnValue = await callback.asyncCall(*args, **kwargs)
             
+        # returns the most recent return value
         return returnValue
     
     def attach(self, func: "function"):
+        # increase callback id
         self.callbackID += 1
 
+        # create callback for the provided function
         currentCallback = callback(
             func = func,
             id = self.callbackID
         )
         
+        # attach it to this event
         currentCallback.attach(self)
 
+        # return the callback
         return currentCallback
     
     def detach(self, callback: "callback"):
         callback.detach()
         return self
-        
+
+# // Callback   
 # function with extra steps
 class callback:
     def __init__(self, func: "function", id: int):
+        # // attributes
         self.func = func
         self.id = id
         self.parent: "event" = None
         self.isAsync = isCoroutine(func)
         
     def attach(self, event: "event"):
+        # set parent attribute
         self.parent = event
+        
+        # attach to parent's callbacks
         self.parent.callbacks.append(self)
+        
+        # return
         return self
         
     def detach(self):
+        # no parent, so can't detach from anything
         if self.parent is None:
             return self
         
+        # remove from parent's callbacks
         self.parent.callbacks.remove(self)
+        
+        # remove parent attribute
+        self.parent = None
+        
+        # return
         return self
         
     def call(self, *args, **kwargs):
+        # check if this is an async callback. if so, return here to prevent calling a coroutine like a regular function
         if self.isAsync:
             return
         
+        # call function
         return self.func(*args, **kwargs)
     
     async def asyncCall(self, *args, **kwargs):
+        # check if this is a normal function. if so, return here to prevent calling a function like a coroutine
         if not self.isAsync:
             return
         
+        # call coroutine
         return await self.func(*args, **kwargs)
