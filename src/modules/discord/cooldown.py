4232405b99f7d207e89795ce0e10ff254cae2e92
Cuh4 <roozzguy@gmail.com>
2023-11-27 17:16:31 +0000

    cooldown uses asyncio instead of threading

--- src/modules/discord/cooldown.py
+++ src/modules/discord/cooldown.py
@@ -4,34 +4,34 @@
 
 # // ---- Imports
 import discord
-import threading
-import time
-
+import asyncio
 # // ---- Variables
 cooldowns = {}
 
 # // ---- Functions
-def __handler(fullKey: str, duration: float|int):
-    time.sleep(duration)
-    cooldowns.pop(fullKey)
-
 def __key(user: discord.User, key: str):
     return str(user.id) + key
 
 def __hasCooldown(fullKey: str):
     return cooldowns.get(fullKey, None) != None
 
+async def __handler(fullKey: str, duration: float|int):
+    await asyncio.sleep(duration)
+    cooldowns.pop(fullKey)
+
 def cooldown(user: discord.User, time: float|int, key: str) -> bool:
+    # get the full key
     fullKey = __key(user, key)
 
+    # check if a cooldown with the same key already exists
     if __hasCooldown(fullKey):
        return True
    
+   # create cooldown
     cooldowns[fullKey] = True
 
-    threading.Thread(
-        target = __handler,
-        args = [fullKey, time]
-    ).start()
+    # remove it after some time
+    asyncio.get_event_loop().run_until_complete(__handler(fullKey, time))
 
+    # return
     return False 
