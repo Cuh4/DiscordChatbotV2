683a635d69418af6953075778f0ce7eba222e60f
Cuh4 <roozzguy@gmail.com>
2023-12-03 22:38:37 +0000

    use typing.any

--- src/modules/general/globals.py
+++ src/modules/general/globals.py
@@ -2,12 +2,15 @@
 # // ------- [Discord Chatbot v2] Globals Module
 # // ---------------------------------------------------------------------
 
+# // ---- Imports
+import typing
+
 # // ---- Variables
-savedGlobals: dict[str, any] = {}
+savedGlobals: dict[str, typing.Any] = {}
 
 # // ---- Functions
-def save(name: str, value: any):
+def save(name: str, value: typing.Any):
     savedGlobals[name] = value
     
-def get(name: str, *, default: any = None):
+def get(name: str, *, default: typing.Any = None):
     return savedGlobals.get(name, default)
